cb31dbb605ca042ea54638723972715d2826b1c5
ProstoDavid <david.kabatov09@gmail.com>
2024-10-26 16:03:21 +0200

    Create Четное-нечетное.cpp

---
+++ Четное-нечетное.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    int a = 0;
+while (a == 0){
+    cin>>t;
+    if(t%2 == 0){
+        cout<<"четное"<<" ";
+    
+    }
+    if(t%2 != 0){
+        cout<<"нечетное"<<" ";
+    }
+    
+}
+}
